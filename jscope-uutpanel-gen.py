053e54670fe451be365f8ed84c143d6826de6afb
dt100 <dt100@naboo.d-tacq>
2021-08-16 10:54:09 +0100

    simplify  Committer: dt100 <dt100@naboo.d-tacq> 	modified:   jscope-uutpanel-gen.py

--- jscope-uutpanel-gen.py
+++ jscope-uutpanel-gen.py
@@ -2,7 +2,7 @@
 
 """
 Example:
-./jscope-flare-gen.py --LHS="$LHS" --RHS="$RHS" $UUTS > ~/jScope/configurations/FLARE_2COL.jscp
+./jscope-uutpanel-gen.py $UUTS > ~/jScope/configurations/FLARE_2COL.jscp
 """
 
 import MDSplus
@@ -12,8 +12,8 @@ import os
 
 def get_args():
     parser = argparse.ArgumentParser(description="jScope file creator.")
-    parser.add_argument('--LHS', default="default", type=str, help="")
-    parser.add_argument('--RHS', default="default", type=str, help="")
+    parser.add_argument('--max_rows', default=8, type=int, help="max rows")
+    parser.add_argument('--node', default="tr", type=str, help="top node")
     parser.add_argument('uuts', nargs='+', help="uut list")
     return parser.parse_args()
 
@@ -29,20 +29,22 @@ def get_default_jscp():
 
 
 def create_new_jscp(args, text):
-    col = 1
+    cols = 1
+    rows = (len(args.uuts),0,)
     new_text = ""
 
-    LHS = len(args.LHS) if args.LHS != "default" else len(args.uuts) - len(args.uuts)//2
-    RHS = len(args.RHS) if args.RHS != "default" else len(args.uuts)//2
+    
+    if len(args.uuts) > args.max_rows:
+        cols = 2
+        rows = (len(args.uuts)/2, len(args.uuts)//2,)
 
-    text += "Scope.plot_1_1.x_expr_1: \TOP:TRANSIENT1:TB_NS\n"
-    text += "Scope.plot_1_1.y_expr_1: \TOP:TRANSIENT1:INPUT_001:CAL_INPUT\n"
+    text += "Scope.plot_1_1.y_expr_1: \TOP:{}:INPUT_001\n".format(args.node)
     text += "Scope.plot_1_1.num_shot: 1\n"
 
     text = text.replace("Scope.rows_in_column_2: 1",
-                        "Scope.rows_in_column_2: {}".format(max([LHS, RHS])))
+                        "Scope.rows_in_column_2: {}".format(rows[1]))
     text = text.replace("Scope.rows_in_column_1: 1",
-                        "Scope.rows_in_column_1: {}".format(max([LHS, RHS])))
+                        "Scope.rows_in_column_1: {}".format(rows[0]))
     text = text.replace("Scope.plot_1_1.num_expr: 0",
                         "Scope.plot_1_1.num_expr: 1")
     text = text.replace("Scope.plot_1_1.global_defaults: -1",
@@ -53,23 +55,24 @@ def create_new_jscp(args, text):
             line_to_add = line.replace("1_1", "{index}_{column}")
             new_text += line_to_add + "\n"
 
-    row = 0
+    row = 1
+    col = 1
     for num, uut in enumerate(args.uuts):
-        if num == LHS:
+        if num == rows[0]:
+            row = 1
             col = 2
-            row = 0
 
         if num != 0:
-            text += new_text.format(index=row+1, column=col)
+            text += new_text.format(index=row, column=col)
         text += "Scope.plot_{}_{}.title: '{} shot: '//$SHOT\n".format(
-            row+1, col, uut.upper())
-        text += "Scope.plot_{}_{}.experiment: {}\n".format(row+1, col, uut.upper())
+            row, col, uut.upper())
+        text += "Scope.plot_{}_{}.experiment: {}\n".format(row, col, uut.upper())
         #text += "Scope.plot_{}_1.num_expr: {}\n".format(row+1, num+1)
-        text += "Scope.plot_{}_{}.mode_1D_1_1: Line\n".format(row+1, col)
-        text += "Scope.plot_{}_{}.mode_2D_1_1: xz(y)\n".format(row+1, col)
-        text += "Scope.plot_{}_{}.color_1_1: 0\n".format(row+1, col)
-        text += "Scope.plot_{}_{}.marker_1_1: 0\n".format(row+1, col)
-        text += "Scope.plot_{}_{}.step_marker_1_1: 1\n\n".format(row+1, col)
+        text += "Scope.plot_{}_{}.mode_1D_1_1: Line\n".format(row, col)
+        text += "Scope.plot_{}_{}.mode_2D_1_1: xz(y)\n".format(row, col)
+        text += "Scope.plot_{}_{}.color_1_1: 0\n".format(row, col)
+        text += "Scope.plot_{}_{}.marker_1_1: 0\n".format(row, col)
+        text += "Scope.plot_{}_{}.step_marker_1_1: 1\n\n".format(row, col)
         row += 1
     print(text)
     return None
@@ -78,9 +81,6 @@ def create_new_jscp(args, text):
 def main():
     args = get_args()
     text = get_default_jscp()
-    if not args.LHS == "default" and not args.RHS == "default":
-        args.LHS = args.LHS.split(" ")
-        args.RHS = args.RHS.split(" ")
     create_new_jscp(args, text)
 
 
